dff809da3deceedd1acd17b6592aaefaf385f2d8
keren man <bebemau@gmail.com>
2015-02-16 22:44:03 -0800

    art in progress

--- kerenm_HW5Drawing/ArtView.m
+++ kerenm_HW5Drawing/ArtView.m
@@ -19,6 +19,15 @@ IB_DESIGNABLE
     [self drawFace];
     [self drawEye:150 PointY:130];
     [self drawEye:250 PointY:130];
+    
+    //left whiskers
+    [self drawWhisker:180 StartY:100 CurveX:2 CurveY:150 Control1X:5 Control1Y:140 Control2X:5 Control2Y:140];
+    [self drawWhisker:180 StartY:100 CurveX:2 CurveY:80 Control1X:0.03 Control1Y:90 Control2X:0.03 Control2Y:80];
+    [self drawWhisker:180 StartY:100 CurveX:2 CurveY:40 Control1X:0.03 Control1Y:50 Control2X:0.03 Control2Y:40];
+    //right whiskers
+    [self drawWhisker:280 StartY:100 CurveX:440 CurveY:150 Control1X:380 Control1Y:140 Control2X:405 Control2Y:140];
+    [self drawWhisker:280 StartY:100 CurveX:440 CurveY:80 Control1X:300 Control1Y:90 Control2X:405 Control2Y:80];
+    [self drawWhisker:280 StartY:100 CurveX:440 CurveY:40 Control1X:410 Control1Y:50 Control2X:405 Control2Y:40];
 }
 
 //-(NSString*)ReturnFormattedNumber:(NSInteger)input StyleFlag : (long) styleSelected {
@@ -54,6 +63,41 @@ IB_DESIGNABLE
     
     [[NSColor blackColor] setFill];
     [circlePath fill];
+}
+
+-(void)drawWhisker: (NSInteger)startX
+            StartY: (NSInteger)startY
+            CurveX: (NSInteger)curveX
+            CurveY: (NSInteger)curveY
+         Control1X: (NSInteger)control1X
+         Control1Y: (NSInteger)control1Y
+         Control2X: (NSInteger)control2X
+         Control2Y: (NSInteger)control2Y
+{
+    NSBezierPath *path = [NSBezierPath bezierPath];
+    [path setLineWidth:1];
+    
+//    [path moveToPoint:NSMakePoint(180, 100)];
+//    
+//    [path curveToPoint:NSMakePoint(2, 80)
+//         controlPoint1:NSMakePoint(0.03, 90)
+//         controlPoint2:NSMakePoint(0.03, 80)
+//     ];
+    
+    [path moveToPoint:NSMakePoint(startX, startY)];
+    
+    [path curveToPoint:NSMakePoint(curveX, curveY)
+         controlPoint1:NSMakePoint(control1X, control1Y)
+         controlPoint2:NSMakePoint(control2X, control2Y)
+     ];
+    
+    [path closePath];
+    
+    [[NSColor colorWithCalibratedWhite:0.98 alpha:1] set];
+    [path fill];
+    
+    [[NSColor colorWithCalibratedWhite:0.6 alpha:1] set];
+    [path stroke];
 }
 
 @end
